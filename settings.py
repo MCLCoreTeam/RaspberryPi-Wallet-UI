8e894bce37af381a5fddbb2c046badd1352e1e52
OneDevGuy <devtrev@protonmail.com>
2018-08-15 02:13:44 +1000

    edit settings config

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ DEBUG = True
 CSRF_ENABLED = True
 CSRF_SESSION_KEY = os.urandom(24)
 SECRET_KEY = os.urandom(24)
-WALLET_RPC_URL = "http://%s:%s@pricebox:8332"%("StakerUI", "1triclypassword!")
+WALLET_RPC_URL = "http://%s:%s@127.0.0.1:8332"%("Rpcuser", "Rpcpassword")
 UI_VERSION = 'v0.1.1'
