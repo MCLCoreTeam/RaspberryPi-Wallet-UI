43a5db31e50424044e3a6abc6d06d5e6a2ada1db
OneDevGuy <devtrev@protonmail.com>
2018-08-14 23:00:36 +1000

    added default settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,6 @@
 import os
 
-DEBUG = True
-
-THREADS_PER_PAGE = 2
-
 CSRF_ENABLED = True
-
 CSRF_SESSION_KEY = os.urandom(24)
-
 SECRET_KEY = os.urandom(24)
-
 WALLET_RPC_URL = "http://%s:%s@127.0.0.1:8332"%("Rpcuser", "Rpcpassword")
