fd86dc33b4ee064a53a3d8d33e0903750af8831c
OneDevGuy <devtrev@protonmail.com>
2018-08-16 14:34:25 +1000

    fixed untracked files

--- settings.py
+++
@@ -1,7 +0,0 @@
-import os
-DEBUG = True
-CSRF_ENABLED = True
-CSRF_SESSION_KEY = os.urandom(24)
-SECRET_KEY = os.urandom(24)
-WALLET_RPC_URL = "http://%s:%s@127.0.0.1:8332"%("Rpcuser", "Rpcpassword")
-UI_VERSION = '0.1.1'
