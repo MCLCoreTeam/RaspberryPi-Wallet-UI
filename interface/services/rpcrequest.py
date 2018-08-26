5fe57b3e86951988d0334bfaf16e46980e17d9af
OneDevGuy <devtrev@protonmail.com>
2018-08-26 22:06:23 +1000

    code clean up

--- interface/services/rpcrequest.py
+++ interface/services/rpcrequest.py
@@ -5,10 +5,12 @@ from . import wallet as rpc_call # This is the flask_wallet_rpc variable
 def wallet_info():
     result = {'immature_coins' : immature_coins(), 'address' : rpc_call.getaccountaddress(''),
             'tx_list' : list_tx()}
-    list = [rpc_call.getwalletinfo(), rpc_call.getnetworkinfo(), rpc_call.getstakinginfo(),
-            rpc_call.getblockchaininfo(), rpc_call.getnettotals()]
+    wallet_list = [
+        rpc_call.getwalletinfo(), rpc_call.getnetworkinfo(), rpc_call.getstakinginfo(),
+        rpc_call.getblockchaininfo(), rpc_call.getnettotals(),
+    ]
 
-    for pairs in list:
+    for pairs in wallet_list:
         result.update(pairs)
 
     time = int(result['expectedtime']) / 60 / 60 / 24
